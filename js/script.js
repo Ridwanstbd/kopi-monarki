ba713b6d619791606c01c3172fe1083471b9f08d
ridwansetio <ridwansetiobudi77@gmail.com>
2023-02-21 23:16:35 +0700

    membuat website kopi monarki

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav')
+document.querySelector('#hamburger-menu').onclick = () =>{
+    navbarNav.classList.toggle('active')
+}
+// klik diluar side bar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu')
+document.addEventListener('click',function(e){
+    if (!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active')
+    }
+})
